3c315337f70f755b30ebc82f9416b5e76efa3f61
Eric Boren <borenet@gmail.com>
2015-08-18 18:16:31 -0400

    Add git config and wrapper script to use Diffuse as a diff tool

---
+++ bin/git_diffuse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# diff is called by git with 7 parameters:
+# path old-file old-hex old-mode new-file new-hex new-mode
+if [ -z "$SSH_CLIENT" ]; then
+  diffuse "$2" "$5" | cat
+else
+  diff "$2" "$5"
+fi
